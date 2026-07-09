3f70d9b447d31abc82893831038725af22169ac4
colls110 <136026626+colls110@users.noreply.github.com>
2026-07-09 18:20:39 -0500

    Update shows.js

--- js/pages/shows.js
+++ js/pages/shows.js
@@ -1,1 +1,21 @@
+import { Header } from "../components/header.js";
 
+export function Shows() {
+
+    return `
+
+        ${Header("shows")}
+
+        <main class="container">
+
+            <h1>📺 Shows</h1>
+
+            <p class="subtitle">
+                Browse all show content.
+            </p>
+
+        </main>
+
+    `;
+
+}
