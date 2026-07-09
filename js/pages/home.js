2d4f8f4a4d8999ac123dfc7fbbd16bf249f2160b
colls110 <136026626+colls110@users.noreply.github.com>
2026-07-09 18:25:54 -0500

    Update home.js

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,13 +1,25 @@
 import { Header } from "../components/header.js";
+import { Card } from "../components/card.js";
+import { videos } from "../data/videos.js";
 
 export function Home() {
 
     return `
+
         ${Header("home")}
 
         <main class="container">
+
             <h1>Welcome!</h1>
+
+            <div class="grid">
+
+                ${videos.map(Card).join("")}
+
+            </div>
+
         </main>
+
     `;
 
 }
