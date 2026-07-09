67057c6df7a421aaa156b7f0780241d29225bafd
colls110 <136026626+colls110@users.noreply.github.com>
2026-07-09 18:14:48 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,3 +5,18 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 window.addEventListener("popstate", router);
+
+document.addEventListener("click", (e) => {
+
+    const link = e.target.closest("[data-link]");
+
+    if (!link)
+        return;
+
+    e.preventDefault();
+
+    history.pushState({}, "", link.href);
+
+    router();
+
+});
