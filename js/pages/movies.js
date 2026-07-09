c8d26cd53cce5ee90b41d47b383a513e86fa6744
colls110 <136026626+colls110@users.noreply.github.com>
2026-07-09 18:20:16 -0500

    Update movies.js

--- js/pages/movies.js
+++ js/pages/movies.js
@@ -1,1 +1,13 @@
+import { Header } from "../components/header.js";
 
+export function Movies() {
+
+    return `
+        ${Header("movies")}
+
+        <main class="container">
+            <h1>Movies</h1>
+        </main>
+    `;
+
+}
