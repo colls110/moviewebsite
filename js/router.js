ab85bfc48da1f723adb808519694a7fc168e9a60
colls110 <136026626+colls110@users.noreply.github.com>
2026-07-09 18:17:58 -0500

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,9 +1,31 @@
 import { Home } from "./pages/home.js";
+import { Movies } from "./pages/movies.js";
+import { Shows } from "./pages/shows.js";
 
 export function router() {
 
     const app = document.getElementById("app");
+    const path = window.location.pathname;
 
-    app.innerHTML = Home();
+    switch (path) {
+
+        case "/":
+            app.innerHTML = Home();
+            break;
+
+        case "/movies":
+            app.innerHTML = Movies();
+            break;
+
+        case "/shows":
+            app.innerHTML = Shows();
+            break;
+
+        default:
+            app.innerHTML = `
+                <h1>404</h1>
+                <p>Page not found.</p>
+            `;
+    }
 
 }
