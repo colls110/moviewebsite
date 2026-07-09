8ee99c7d410fbef0c73998e7c17e0df0b468e03c
colls110 <136026626+colls110@users.noreply.github.com>
2026-07-09 18:04:13 -0500

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,10 +1,7 @@
+import { Home } from "./pages/home.js";
+
 export function router() {
 
-    const app = document.getElementById("app");
-
-    app.innerHTML = `
-        <h1>C's Movies</h1>
-        <p>Router is working!</p>
-    `;
+    document.getElementById("app").innerHTML = Home();
 
 }
